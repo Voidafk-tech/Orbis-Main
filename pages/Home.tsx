98387b97e0c51ef877e2d5545cbe660a7b313386
Godtfred Feng <122638864+Voidafk-tech@users.noreply.github.com>
2026-07-23 15:12:57 -0700

    Refactor Home component by removing unused sections
    Removed Pricing, Process, and FAQ components from Home page.

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import Hero from '../components/Hero';
 import Services from '../components/Services';
-import Process from '../components/Process';
-import FAQ from '../components/FAQ';
 import CTA from '../components/CTA';
-import Pricing from '../components/Pricing';
 
 const Home: React.FC = () => {
   return (
     <>
       <Hero />
+      <hr className="border-t border-white/5 max-w-6xl mx-auto" />
       <Services />
-      <Pricing />
-      <Process />
-      <FAQ />
+      <hr className="border-t border-white/5 max-w-6xl mx-auto" />
       <CTA />
     </>
   );
